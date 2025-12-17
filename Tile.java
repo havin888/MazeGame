77f879536afb3f263a5501b5ad5f7eb27359f6a9
nermintuncbilek <nermin5832@gmail.com>
2025-12-17 10:34:14 +0300

    Backend related codes and classes added.

---
+++ Tile.java
@@ -0,0 +1,43 @@
+public class Tile {
+    private Position position;
+    private TileType type;
+    
+    public Tile(Position position, TileType type) {
+        this.position = position;
+        this.type = type;
+    }
+    
+    public Position getPosition() {
+        return position;
+    }
+    
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+    
+    public TileType getType() {
+        return type;
+    }
+    
+    public void setType(TileType type) {
+        this.type = type;
+    }
+    
+    public boolean canPlayerPass(PlayerId playerId) {
+        switch (type) {
+            case FLOOR:
+                return true;
+            case GOAL:
+                // CRITICAL: Goal must be accessible to BOTH players
+                return true;
+            case REGULAR_WALL:
+                return false;
+            case BLUE_WALL:
+                return playerId == PlayerId.PLAYER_1;
+            case RED_WALL:
+                return playerId == PlayerId.PLAYER_2;
+            default:
+                return false;
+        }
+    }
+}
